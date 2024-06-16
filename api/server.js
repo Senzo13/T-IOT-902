3817d563b6ba6f5ae47f3f5a951eaf9da6e187e1
Lorenzo <52446531+Senzo13@users.noreply.github.com>
2024-06-16 19:52:34 +0200

    added: main.ino

--- api/server.js
+++ api/server.js
@@ -1,8 +1,38 @@
+const fs = require("fs");
+const http = require("http");
+const https = require("https");
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
 const port = 3062;
+
+const envInfo = {
+  version: process.env.NODE_ENV || "development",
+};
+
+let server;
+
+if (envInfo.version === "production" || envInfo.version === "test") {
+  // Définir le chemin du fichier PFX en fonction de l'environnement
+  let pfxPath;
+
+  if (envInfo.version === "production") {
+    pfxPath =
+      "C:\\ProgramData\\certify\\assets\\iot-902.lgiralt.com\\20240914_fb116436.pfx";
+  } else if (envInfo.version === "test") {
+    pfxPath =
+      "C:\\ProgramData\\certify\\assets\\iot-902.lgiralt.com\\20240914_fb116436.pfx";
+  }
+
+  const options = {
+    pfx: fs.readFileSync(pfxPath),
+  };
+
+  server = https.createServer(options, app);
+} else {
+  server = http.createServer(app);
+}
 
 app.use(bodyParser.json());
 
@@ -20,6 +50,12 @@ app.post("/ttn-webhook", (req, res) => {
   res.status(200).send("Data received");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+server.listen(port, () => {
+  console.log(
+    `Server is running on ${
+      envInfo.version === "production" || envInfo.version === "test"
+        ? "https"
+        : "http"
+    }://localhost:${port}`
+  );
 });
---
+++ lib/main.ino
@@ -0,0 +1,45 @@
+#include "heltec.h"
+
+int dustSensorPin = 36;
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println("Initializing OLED...");
+
+    Heltec.begin(true, false, true);
+
+    // Vérifier si l'écran OLED est initialisé
+    if (!Heltec.display) {
+        Serial.println("OLED initialization failed!");
+        while (1);
+    }
+
+    // Afficher un message sur l'écran OLED
+    Heltec.display->clear();
+    Heltec.display->setFont(ArialMT_Plain_10);
+    Heltec.display->drawString(0, 0, "Dust Sensor Reading");
+    Heltec.display->display();
+    Serial.println("OLED initialized successfully!");
+
+    // Configurer la broche du capteur de poussière
+    pinMode(dustSensorPin, INPUT);
+}
+
+void loop() {
+    // Lire la valeur du capteur de poussière
+    int dustValue = analogRead(dustSensorPin);
+
+    // Afficher la valeur lue sur le moniteur série
+    Serial.print("Dust Sensor Value: ");
+    Serial.println(dustValue);
+
+    // Afficher la valeur sur l'écran OLED
+    Heltec.display->clear();
+    Heltec.display->setFont(ArialMT_Plain_10);
+    Heltec.display->drawString(0, 0, "Dust Sensor Value:");
+    Heltec.display->drawString(0, 10, String(dustValue));
+    Heltec.display->display();
+
+    // Attendre 1 seconde avant de lire à nouveau
+    delay(1000);
+}
